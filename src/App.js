9b473eeee9f549137b7afeb1c4e620d405fa96c7
jordandde <ja3desou@uwaterloo.ca>
2019-11-11 19:45:50 -0500

    finished testing for today

--- src/App.js
+++ src/App.js
@@ -118,7 +118,7 @@ function App() {
           <Jordandde>Jordandde</Jordandde>
           <Text>About Me</Text>
         </Banner>
-        <p>{date === "Mon/7" ? date : "Loading date..."}</p>
+        {/*<p>{date === "Mon/7" ? date : "Loading date..."}</p>*/}
         <Center>
           <Square
             style={{

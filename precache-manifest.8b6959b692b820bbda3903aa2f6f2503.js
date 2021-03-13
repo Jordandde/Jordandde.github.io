e68702832938d44b8f9b25a7ce4de06d895cddc4
Jordan de Souza <jordandesouza@Jordans-MacBook-Pro.local>
2021-03-12 20:38:19 -0500

    update

---
+++ precache-manifest.8b6959b692b820bbda3903aa2f6f2503.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1f02e7c1d3804780027",
+    "url": "/static/css/main.2d6def63.chunk.css"
+  },
+  {
+    "revision": "a1f02e7c1d3804780027",
+    "url": "/static/js/main.a1f02e7c.chunk.js"
+  },
+  {
+    "revision": "1230f11944b2cfe1df9c",
+    "url": "/static/js/1.1230f119.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cae3f7223d6af5107d41d9f4c5136ea6",
+    "url": "/index.html"
+  }
+];
